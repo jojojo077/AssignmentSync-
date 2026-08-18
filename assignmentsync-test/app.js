d25f216f665354f2ffc0976e9c43e321ef9dbf32
Dorp <46204816+jojojo077@users.noreply.github.com>
2026-08-18 17:54:50 +1200

    Add more methods

--- assignmentsync-test/app.js
+++ assignmentsync-test/app.js
@@ -15,17 +15,27 @@ async function getCourses() {
 
     
     console.log('################################################################################');
-    // retrieve current year courses
+    // retrieve current year courses ICS file
+    const calendar_list = [];
     courses.forEach(course => {
         if (course.name !== "undefined" && course.course_code) {
-            if (course.course_code.includes("2026")) {
-                console.log(`${course.name}`);
+            if (course.course_code.includes("2026") && course.course_code.includes("S2")) {
+                calendar_list.push(course.course_code + " " + course.calendar.ics);
             }
         }
         
     });
+    calendar_list.forEach(element => {
+        console.log(element);
+    });
+    console.log('################################################################################');
+    // search assignments in course
+    const courseCodeSearch = "ENSE707";
+    courses.forEach(course => {
+        if (course.name !== "undefined" && course.course_code) {
 
-
+        }
+    })
 }
 
 getCourses();
